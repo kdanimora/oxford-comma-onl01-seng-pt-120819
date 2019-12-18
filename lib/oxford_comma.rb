495d2fc713b1fa7b2b701742986ce43f17c06a0f
Karen Daniela <kdaniimora@gmail.com>
2019-12-18 15:05:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     return array.join(" and ")
  
   elsif array.length > 2
-    array.insert(-2, "and ")
+    array[-1].insert(0, "and ")
     return array.join(", ")
     
   end 
